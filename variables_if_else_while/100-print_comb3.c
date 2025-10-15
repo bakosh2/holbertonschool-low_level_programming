d4e4ec33a6f4311ee729a597140afa01cb264980
bakosh2 <bakosh.2@hotmail.com>
2025-10-15 16:55:17 +0000

    Add 100-print_comb3.c advanced program

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int first, second;
+
+	for (first = 0; first < 9; first++)
+	{
+		for (second = first + 1; second < 10; second++)
+		{
+			putchar(first + '0');
+			putchar(second + '0');
+
+			if (first != 8 || second != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
