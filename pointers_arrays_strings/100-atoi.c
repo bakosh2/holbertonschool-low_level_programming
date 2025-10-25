e8f4c14eccbb6e0d1dcc0c4e5813fc52ecafd989
bakosh2 <bakosh2@hotmail.com>
2025-10-25 05:44:10 +0000

    Fix: _atoi with overflow handling

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,17 +10,32 @@ int _atoi(char *s)
 	int result = 0;
 	int sign = 1;
 	int i = 0;
+	int digit;
 
-	while (s[i] && (s[i] < '0' || s[i] > '9'))
+	/* Skip non-digit characters and handle signs */
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		i++;
 	}
 
+	/* Convert digits to integer with overflow check */
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		result = result * 10 + (s[i] - '0');
+		digit = s[i] - '0';
+		
+		/* Check for positive overflow */
+		if (sign == 1 && (result > 214748364 || 
+			(result == 214748364 && digit > 7)))
+			return (2147483647);
+		
+		/* Check for negative overflow */
+		if (sign == -1 && (result > 214748364 || 
+			(result == 214748364 && digit > 8)))
+			return (-2147483648);
+		
+		result = result * 10 + digit;
 		i++;
 	}
 
