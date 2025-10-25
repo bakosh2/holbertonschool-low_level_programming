6caeef3cdd4b3634d89f80e5f084b176f9310642
bakosh2 <bakosh2@hotmail.com>
2025-10-25 05:52:09 +0000

    Task 100: Simple working _atoi implementation

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,41 +1,31 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to convert
+ * _atoi - converts string to integer
+ * @s: input string
  * Return: converted integer
  */
 int _atoi(char *s)
 {
-	unsigned int num = 0;
+	int result = 0;
 	int sign = 1;
-	int started = 0;
+	int i = 0;
 
-	while (*s)
+	/* Skip spaces and handle signs */
+	while (s[i] == ' ' || s[i] == '+' || s[i] == '-')
 	{
-		if (*s == '-')
-			sign *= -1;
-
-		if (*s >= '0' && *s <= '9')
-		{
-			started = 1;
-			/* Check for overflow before actually causing it */
-			if (num > (2147483647 - (*s - '0')) / 10)
-			{
-				if (sign == 1)
-					return (2147483647);
-				else
-					return (-2147483648);
-			}
-			num = num * 10 + (*s - '0');
-		}
-		else if (started)
-		{
-			/* Stop if we already started converting numbers */
-			break;
-		}
-		s++;
+		if (s[i] == '-')
+			sign = -sign;
+		i++;
 	}
 
-	return (num * sign);
+	/* Convert numbers */
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		/* Simple conversion without complex overflow checks */
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (sign * result);
 }
