6431fc5dad47d74dc9c232706a77b1de6d8b6c1f
bakosh2 <bakosh2@hotmail.com>
2025-11-08 19:41:49 +0000

    Task 0: Define struct dog with name, age, owner

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure representing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This structure defines a dog with its name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
