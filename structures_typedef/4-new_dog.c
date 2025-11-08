d7480a7b760e87d7899bd1a234c0d6f22a69849a
bakosh2 <bakosh2@hotmail.com>
2025-11-08 20:50:05 +0000

    Fix: Betty style compliance - use tabs instead of spaces

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,46 +11,46 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *name_copy, *owner_copy;
-	int name_len = 0, owner_len = 0, i;
+	dog_t *d;
+	char *n, *o;
+	int i, nl = 0, ol = 0;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
-	while (name[name_len])
-		name_len++;
-	while (owner[owner_len])
-		owner_len++;
+	while (name[nl])
+		nl++;
+	while (owner[ol])
+		ol++;
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
 		return (NULL);
 
-	name_copy = malloc(name_len + 1);
-	if (name_copy == NULL)
+	n = malloc(nl + 1);
+	if (n == NULL)
 	{
-		free(new_dog);
+		free(d);
 		return (NULL);
 	}
 
-	for (i = 0; i <= name_len; i++)
-		name_copy[i] = name[i];
-
-	owner_copy = malloc(owner_len + 1);
-	if (owner_copy == NULL)
+	o = malloc(ol + 1);
+	if (o == NULL)
 	{
-		free(name_copy);
-		free(new_dog);
+		free(n);
+		free(d);
 		return (NULL);
 	}
 
-	for (i = 0; i <= owner_len; i++)
-		owner_copy[i] = owner[i];
+	for (i = 0; i <= nl; i++)
+		n[i] = name[i];
 
-	new_dog->name = name_copy;
-	new_dog->age = age;
-	new_dog->owner = owner_copy;
+	for (i = 0; i <= ol; i++)
+		o[i] = owner[i];
 
-	return (new_dog);
+	d->name = n;
+	d->age = age;
+	d->owner = o;
+
+	return (d);
 }
