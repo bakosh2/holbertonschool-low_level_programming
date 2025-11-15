983b799639ccc8993f8de061449c785e26be171f
“bakosh2” <bakosh2@hotmail.com>
2025-11-15 16:25:23 +0000

    Fix: Betty style compliance for calculator project

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -3,14 +3,13 @@
 
 /**
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -20,4 +19,4 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
-#endif /* CALC_H */
+#endif
