ec211753bdfad86fbe40179ddd7c3ac51fef9ae4
“bakosh2” <bakosh2@hotmail.com>
2025-11-27 11:16:47 +0000

    create and write in 2-add_dnodeint.c and lists.h file

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,27 +1,28 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
- * struct dlistint_s - doubly linked list
- * @n: integer stored in the node
- * @prev: points to the previous node
+ * struct list_s - singly linked list
+ * @str: string - malloc'd string
+ * @len: length of the string
  * @next: points to the next node
- *
- * Description: doubly linked list node structure
  */
-typedef struct dlistint_s
+typedef struct list_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
-} dlistint_t;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
-size_t print_dlistint(const dlistint_t *h);
-size_t dlistint_len(const dlistint_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+size_t list_len(const list_t *h);
+void free_list(list_t *head);
+size_t print_list(const list_t *h);
 
 #endif
 
