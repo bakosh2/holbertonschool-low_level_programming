113d016bf2457579b49aa4c3df2827f1f9d5b8a1
“bakosh2” <bakosh2@hotmail.com>
2025-11-27 11:21:22 +0000

    fix lists.h file

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,28 +1,28 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * struct list_s - singly linked list
- * @str: string - malloc'd string
- * @len: length of the string
+ * struct dlistint_s - doubly linked list node
+ * @n: integer
+ * @prev: points to the previous node
  * @next: points to the next node
  */
-typedef struct list_s
+typedef struct dlistint_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-size_t list_len(const list_t *h);
-void free_list(list_t *head);
-size_t print_list(const list_t *h);
+/* Prototypes */
+size_t print_dlistint(const dlistint_t *h);
+size_t _print_dlistint_backward(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+void _free_dlist(dlistint_t *head);
 
-#endif
+#endif /* LISTS_H */
 
