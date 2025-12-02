a86404b6235a3678f4ee86924ebe1a71a62aaf86
“bakosh2” <bakosh2@hotmail.com>
2025-12-02 14:39:15 +0000

    write in the lists.h file

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -28,4 +28,6 @@ void free_dlistint(dlistint_t *head);
 size_t print_dlistint_backward(const dlistint_t *h);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+
 #endif
