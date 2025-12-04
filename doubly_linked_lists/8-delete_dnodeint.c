a2051f82dc6106ea1321181f1761d6c9ebac1764
“bakosh2” <bakosh2@hotmail.com>
2025-12-04 14:07:50 +0000

    write the code

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes a node at a given index
+ * @head: pointer to pointer to head
+ * @index: index of node to delete
+ * Return: 1 if success, -1 if failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp = *head;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = tmp->next;
+		if (tmp->next)
+			tmp->next->prev = NULL;
+		free(tmp);
+		return (1);
+	}
+
+	while (tmp && i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	if (!tmp)
+		return (-1);
+
+	if (tmp->next)
+		tmp->next->prev = tmp->prev;
+
+	if (tmp->prev)
+		tmp->prev->next = tmp->next;
+
+	free(tmp);
+	return (1);
+}
+ 
